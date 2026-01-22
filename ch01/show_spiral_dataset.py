ce85970bbdc3d017cc35a7f8b752765435b9bdf7
이하은 <112817806+amou6518@users.noreply.github.com>
2026-01-22 21:07:57 +0900

    1주차 신경망 복습
    1주차 학습인증

---
+++ ch01/show_spiral_dataset.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+import sys,os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.join(current_dir, '..')
+sys.path.append(parent_dir)
+from dataset import spiral
+import matplotlib.pyplot as plt
+
+
+x, t = spiral.load_data()
+print('x', x.shape)  # (300, 2)
+print('t', t.shape)  # (300, 3)
+
+# 데이터점 플롯
+N = 100
+CLS_NUM = 3
+markers = ['o', 'x', '^']
+for i in range(CLS_NUM):
+    plt.scatter(x[i*N:(i+1)*N, 0], x[i*N:(i+1)*N, 1], s=40, marker=markers[i])
+plt.show()
