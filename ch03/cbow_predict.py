798fe7dcde7cb24d08cb9a4bba322bbd109af116
이하은 <112817806+amou6518@users.noreply.github.com>
2026-01-22 23:53:02 +0900

    3주차 실습 코드

---
+++ ch03/cbow_predict.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+import sys,os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.join(current_dir, '..')
+sys.path.append(parent_dir)
+import numpy as np
+from common.layers import MatMul
+
+
+# 샘플 맥락 데이터
+c0 = np.array([[1, 0, 0, 0, 0, 0, 0]])
+c1 = np.array([[0, 0, 1, 0, 0, 0, 0]])
+
+# 가중치 초기화
+W_in = np.random.randn(7, 3)
+W_out = np.random.randn(3, 7)
+
+# 계층 생성
+in_layer0 = MatMul(W_in)
+in_layer1 = MatMul(W_in)
+out_layer = MatMul(W_out)
+
+# 순전파
+h0 = in_layer0.forward(c0)
+h1 = in_layer1.forward(c1)
+h = 0.5 * (h0 + h1)
+s = out_layer.forward(h)
+print(s)
