2b798412c544f7e2f20a948eafe4b6675b398ae6
이하은 <112817806+amou6518@users.noreply.github.com>
2026-01-31 00:43:09 +0900

    4주차 실습코드
    4주차 실습코드

---
+++ ch04/eval.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+import sys,os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.join(current_dir, '..')
+sys.path.append(parent_dir)
+from common.util import most_similar, analogy
+import pickle
+
+
+pkl_file = 'cbow_params.pkl'
+# pkl_file = 'skipgram_params.pkl'
+
+with open(pkl_file, 'rb') as f:
+    params = pickle.load(f)
+    word_vecs = params['word_vecs']
+    word_to_id = params['word_to_id']
+    id_to_word = params['id_to_word']
+
+# 가장 비슷한(most similar) 단어 뽑기
+querys = ['you', 'year', 'car', 'toyota']
+for query in querys:
+    most_similar(query, word_to_id, id_to_word, word_vecs, top=5)
+
+# 유추(analogy) 작업
+print('-'*50)
+analogy('king', 'man', 'queen',  word_to_id, id_to_word, word_vecs)
+analogy('take', 'took', 'go',  word_to_id, id_to_word, word_vecs)
+analogy('car', 'cars', 'child',  word_to_id, id_to_word, word_vecs)
+analogy('good', 'better', 'bad',  word_to_id, id_to_word, word_vecs)
