94e13062691a714c965e595272c63105eec409ea
이하은 <112817806+amou6518@users.noreply.github.com>
2026-02-13 23:59:47 +0900

    6주차 실습코드

---
+++ ch06/eval.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+import sys,os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.join(current_dir, '..')
+sys.path.append(parent_dir)
+from rnnlm import Rnnlm
+from better_rnnlm import BetterRnnlm
+from dataset import ptb
+from common.util import eval_perplexity
+
+
+if __name__ == '__main__':
+    model = Rnnlm()
+    #model = BetterRnnlm()
+
+    # 학습된 매개변수 읽기
+    model.load_params()
+
+    corpus, _, _ = ptb.load_data('test')
+
+    model.reset_state()
+    ppl_test = eval_perplexity(model, corpus)
+    print('test perplexity: ', ppl_test)
